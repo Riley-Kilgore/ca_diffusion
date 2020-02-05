63a41b9d99d6135fc32fde786654a480ac61f565
Victor Suciu <victor@Victors-MacBook-Pro-4.local>
2020-02-04 16:50:20 -0800

    fixed initBar

--- initBar.py
+++ initBar.py
@@ -1,6 +1,11 @@
 import numpy as np
+from applyHotCold import applyHotCold
 
 # creates extra space (1-cell border) for boundery
-def initBar(width, height):
-    return np.zeros((height + 2, width + 2), dtype=float)
+def initBar(width, height, hotsites, coldsites):
+    amb_temp = 20
+    bar = np.ones((height + 2, width + 2), dtype=float)
+    bar = bar * amb_temp
+
+    return applyHotCold(amb_temp, hotsites, coldsites)
 
