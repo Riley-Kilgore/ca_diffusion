7dee6d91281fc04b466c61719b2d0a14d7065e77
Riley Kilgore <riley-kilgore@github.com>
2020-02-04 16:34:26 -0800

    Actually working now hopefully...

--- applyDiffusionExtended.py
+++ applyDiffusionExtended.py
@@ -1,2 +1,12 @@
 import numpy as N
+from Diffusion import diffusion
 
+def applyDiffusionExtended(ext_lat, diffusion_rate):
+    columns = ext_lat.size()[1]
+    rows = ext_lat.size()[0]
+    new_lat = N.zeros((rows-2,columns-2))
+    for i in range(1,rows-1):
+        for j in range(1,columns-1):
+            new_lat[i,j] = diffusion(diffusion_rate, ext_lat[i,j], ext_lat[i-1,j], ext_lat[i-1,j+1], ext_lat[i,j+1],\
+                ext_lat[i+1,j+1], ext_lat[i+1,j], ext_lat[i+1,j-1], ext_lat[i,j-1], ext_lat[i-1,j-1])
+    return new_lat
